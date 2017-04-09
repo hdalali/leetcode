430e6a3dfee390cc442b4a7c624da3df2174772e
hdalali <dallali.harsha@gmail.com>
2017-04-09 03:32:54 -0700

    changes

--- src/crackingCodingInterview/c1a1.java
+++ src/crackingCodingInterview/c1a1.java
@@ -3,27 +3,7 @@ import java.util.*;
 
 public class c1a1 {
 	public static void uniq(){
-		/*
-		String s=new String();
-		s = "abcdefg";
-		char c1,c2;
-		int i,j;
-		for(i=0;i<s.length();i++){
-			c1 = s.charAt(i);
-			for(j=0;j<s.length();j++){
-				if(i == j){
-					continue;
-				}
-				
-				c2 = s.charAt(j);
-				if(c1 == c2){
-					System.out.println("String does not have all unique characters");
-					return;
-				}
-			}
-		}
-		System.out.println("String has ALL unique characters");
-		*/
+
 		boolean res = isuniq("abcdefgg");
 				if(res == true){
 					System.out.println("String HAS ALL unique characters");
