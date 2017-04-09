513c0bdbd976be631422f5ee3260bc648d9886ea
hdalali <dallali.harsha@gmail.com>
2017-04-09 05:50:23 -0700

    Added Valid Anagram Program

---
+++ src/crackingCodingInterview/ValidAnagram.java
@@ -0,0 +1,18 @@
+package crackingCodingInterview;
+
+import java.util.HashMap;
+
+public class ValidAnagram {
+	public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        HashMap<Character,Integer> hm=new HashMap<Character,Integer>();
+        for(int i=0;i<s.length();i++){
+            hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i=0;i<t.length();i++){
+            hm.put(t.charAt(i),hm.getOrDefault(t.charAt(i),0)-1);
+            if(hm.get(t.charAt(i))==0) hm.remove(t.charAt(i));
+        }
+        return hm.isEmpty();
+    }
+}
